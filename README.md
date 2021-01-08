12520a977bb3994b7bf6e4dac2463571d3c61eaf
xwyang <xiaowei.yang@crgecent.com>
2021-01-08 18:46:29 +0800

    添加评论插件

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 基于Vue-Press搭建的静态博客，腾讯云托管。主要分享Java相关内容。
+
+[预览地址](https://www.studying.icu/)
--- vuepress/.vuepress/config/plugins.js
+++ vuepress/.vuepress/config/plugins.js
@@ -6,38 +6,9 @@ module.exports = [
   //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
   // }],
 
-  'vuepress-plugin-baidu-autopush', // 百度自动推送
 
   // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-  [
-    'thirdparty-search',
-    {
-      thirdparty: [
-        // 可选，默认 []
-        {
-          title: '在MDN中搜索',
-          frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-          behindUrl: '', // 搜索链接的后面部分，可选，默认 ''
-        },
-        {
-          title: '在Runoob中搜索',
-          frontUrl: 'https://www.runoob.com/?s=',
-        },
-        {
-          title: '在Vue API中搜索',
-          frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        },
-        {
-          title: '在Bing中搜索',
-          frontUrl: 'https://cn.bing.com/search?q=',
-        },
-        {
-          title: '通过百度搜索本站的',
-          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
-        },
-      ],
-    },
-  ],
+
 
   [
     'one-click-copy',
@@ -47,20 +18,6 @@ module.exports = [
       copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
       duration: 1000, // prompt message display time.
       showInMobile: false, // whether to display on the mobile side, default: false.
-    },
-  ],
-  [
-    'demo-block',
-    {
-      // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
-      settings: {
-        // jsLib: ['http://xxx'], // 在线示例(jsfiddle, codepen)中的js依赖
-        // cssLib: ['http://xxx'], // 在线示例中的css依赖
-        // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
-        jsfiddle: false, // 是否显示 jsfiddle 链接
-        codepen: true, // 是否显示 codepen 链接
-        horizontal: false, // 是否展示为横向样式
-      },
     },
   ],
   [
@@ -78,23 +35,38 @@ module.exports = [
       hm: 'e8e310c0a2588005c9569e6b383d8f2c',
     },
   ],
+  [
+
+    '@vssue/vuepress-plugin-vssue', // 评论
+    {
+      platform: 'github-v4', //v3的platform是github，v4的是github-v4
+      locale: 'zh', //语言
+      // 其他的 Vssue 配置
+      owner: 'yxw839841231', //github账户名
+      repo: 'studying-comment', //github一个项目的名称
+      clientId: '2157eb165ce0edbddfbe',//注册的Client ID
+      clientSecret: '02afd73fd3d3034bf9f34c71d05e93ba5b69f544',//注册的Client Secret
+      autoCreateIssue:true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
+
+    },
+  ],
   // [
   //   'vuepress-plugin-comment', // 评论
   //   {
   //     choosen: 'gitalk',
   //     options: {
-  //       clientID: 'a6e1355287947096b88b',
-  //       clientSecret: 'f0e77d070fabfcd5af95bebb82b2d574d7248d71',
-  //       repo: 'blog-gitalk-comment', // GitHub 仓库
-  //       owner: 'xugaoyi', // GitHub仓库所有者
-  //       admin: ['xugaoyi'], // 对仓库有写权限的人
+  //       clientID: '2157eb165ce0edbddfbe',
+  //       clientSecret: '02afd73fd3d3034bf9f34c71d05e93ba5b69f544',
+  //       repo: 'studying-comment', // GitHub 仓库
+  //       owner: 'yxw839841231', // GitHub仓库所有者
+  //       admin: ['yxw839841231'], // 对仓库有写权限的人
   //       // distractionFreeMode: true,
   //       pagerDirection: 'last', // 'first'正序 | 'last'倒序
   //       id: '<%- (frontmatter.permalink || frontmatter.to.path).slice(-16) %>', //  页面的唯一标识,长度不能超过50
   //       title: '「评论」<%- frontmatter.title %>', // GitHub issue 的标题
   //       labels: ['Gitalk', 'Comment'], // GitHub issue 的标签
   //       body:
-  //         '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
+  //           '页面：<%- window.location.origin + (frontmatter.to.path || window.location.pathname) %>', // GitHub issue 的内容
   //     },
   //   },
   // ],
