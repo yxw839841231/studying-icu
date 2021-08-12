3714119ae2d3da287a2e4e60aedcf5493ae555bb
xwyang <xiaowei.yang@crgecent.com>
2021-08-12 10:04:35 +0800

    增加

---
+++ vuepress/.vuepress/components/ArticleTopAd.vue
@@ -0,0 +1,9 @@
+<template>
+  <InArticleAdsense
+      data-ad-client="ca-pub-5656294142997372"
+      data-ad-slot="1631732639"></InArticleAdsense>
+</template>
+
+<script>
+export default {};
+</script>
